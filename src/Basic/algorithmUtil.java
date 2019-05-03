e0d91ea5be6f71d8290101820ad9ee5820887852
Jewpigeon <43291850+Jewpigeon@users.noreply.github.com>
2019-05-03 15:11:35 +0300

    Update algorithmUtil.java

--- src/Basic/algorithmUtil.java
+++ src/Basic/algorithmUtil.java
@@ -1,5 +1,7 @@
 package Basic;
 
+import java.util.Comparator;
+
 public class algorithmUtil {
 
     public static void reverseArray(byte[] input) {
@@ -11,6 +13,7 @@ public class algorithmUtil {
         }
 
     }
+
     public static void reverseArray(short[] input) {
         short tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
@@ -20,6 +23,7 @@ public class algorithmUtil {
         }
 
     }
+
     public static void reverseArray(int[] input) {
         int tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
@@ -29,6 +33,7 @@ public class algorithmUtil {
         }
 
     }
+
     public static void reverseArray(long[] input) {
         long tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
@@ -38,6 +43,7 @@ public class algorithmUtil {
         }
 
     }
+
     public static void reverseArray(char[] input) {
         char tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
@@ -47,7 +53,8 @@ public class algorithmUtil {
         }
 
     }
-    public static void reverseArray(double [] input) {
+
+    public static void reverseArray(double[] input) {
         double tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -56,7 +63,8 @@ public class algorithmUtil {
         }
 
     }
-    public static void reverseArray(float [] input) {
+
+    public static void reverseArray(float[] input) {
         float tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -65,7 +73,8 @@ public class algorithmUtil {
         }
 
     }
-    public static void reverseArray(boolean [] input) {
+
+    public static void reverseArray(boolean[] input) {
         boolean tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -74,6 +83,7 @@ public class algorithmUtil {
         }
 
     }
+
     public static <Type> void reverseArray(Type[] input) {
         Type tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
@@ -84,7 +94,7 @@ public class algorithmUtil {
     }
 
 
-    public static void reverseMatrix(byte[][] input){
+    public static void reverseMatrix(byte[][] input) {
         byte[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -92,7 +102,8 @@ public class algorithmUtil {
             input[input.length - i - 1] = tmp;
         }
     }
-    public static void reverseMatrix(short[][] input){
+
+    public static void reverseMatrix(short[][] input) {
         short[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -100,7 +111,8 @@ public class algorithmUtil {
             input[input.length - i - 1] = tmp;
         }
     }
-    public static void reverseMatrix(int[][] input){
+
+    public static void reverseMatrix(int[][] input) {
         int[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -108,7 +120,8 @@ public class algorithmUtil {
             input[input.length - i - 1] = tmp;
         }
     }
-    public static void reverseMatrix(long[][] input){
+
+    public static void reverseMatrix(long[][] input) {
         long[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -116,7 +129,8 @@ public class algorithmUtil {
             input[input.length - i - 1] = tmp;
         }
     }
-    public static void reverseMatrix(float[][] input){
+
+    public static void reverseMatrix(float[][] input) {
         float[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -124,7 +138,8 @@ public class algorithmUtil {
             input[input.length - i - 1] = tmp;
         }
     }
-    public static void reverseMatrix(double[][] input){
+
+    public static void reverseMatrix(double[][] input) {
         double[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -132,7 +147,8 @@ public class algorithmUtil {
             input[input.length - i - 1] = tmp;
         }
     }
-    public static void reverseMatrix(char[][] input){
+
+    public static void reverseMatrix(char[][] input) {
         char[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -140,7 +156,8 @@ public class algorithmUtil {
             input[input.length - i - 1] = tmp;
         }
     }
-    public static void reverseMatrix(boolean[][] input){
+
+    public static void reverseMatrix(boolean[][] input) {
         boolean[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -148,7 +165,8 @@ public class algorithmUtil {
             input[input.length - i - 1] = tmp;
         }
     }
-    public static <Type> void reverseMatrix(Type[][] input){
+
+    public static <Type> void reverseMatrix(Type[][] input) {
         Type[] tmp;
         for (int i = 0; i < input.length >> 1; ++i) {
             tmp = input[i];
@@ -158,69 +176,143 @@ public class algorithmUtil {
     }
 
 
-    public static int indexOfElement(byte[] input, byte element){
+    public static int indexOfElement(byte[] input, byte element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == element) return i;
+            if (input[i] == element) return i;
         }
         return -1;
     }
-    public static int indexOfElement(short[] input, short element){
+
+    public static int indexOfElement(short[] input, short element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == element) return i;
+            if (input[i] == element) return i;
         }
         return -1;
     }
-    public static int indexOfElement(int[] input, int element){
+
+    public static int indexOfElement(int[] input, int element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == element) return i;
+            if (input[i] == element) return i;
         }
         return -1;
     }
-    public static int indexOfElement(long[] input, long element){
+
+    public static int indexOfElement(long[] input, long element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == element) return i;
+            if (input[i] == element) return i;
         }
         return -1;
     }
-    public static int indexOfElement(float[] input, float element){
+
+    public static int indexOfElement(float[] input, float element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == element) return i;
+            if (input[i] == element) return i;
         }
         return -1;
     }
-    public static int indexOfElement(double[] input, double element){
+
+    public static int indexOfElement(double[] input, double element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == element) return i;
+            if (input[i] == element) return i;
         }
         return -1;
     }
-    public static int indexOfElement(char[] input, char element){
+
+    public static int indexOfElement(char[] input, char element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == element) return i;
+            if (input[i] == element) return i;
         }
         return -1;
     }
-    public static int indexOfElement(boolean[] input, boolean element){
+
+    public static int indexOfElement(boolean[] input, boolean element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == element) return i;
+            if (input[i] == element) return i;
         }
         return -1;
     }
-    public static <Type> int indexOfElement(Type[] input, Type element){
+
+    public static <Type> int indexOfElement(Type[] input, Type element) {
         for (int i = 0; i < input.length; i++) {
-            if(input[i].equals(element)) return i;
+            if (input[i].equals(element)) return i;
         }
         return -1;
     }
 
 
+    public static int binarySearch(byte[] array, byte seek, int startpos, int endpos) {
+        if (startpos == endpos) return -1;
+        int l2 = startpos + (Math.abs(startpos - endpos) / 2);
+        if (array[l2] == seek) return l2;
+        if (seek > array[l2]) return binarySearch(array, seek, l2 + 1, endpos);
+        if (seek < array[l2]) return binarySearch(array, seek, startpos, l2);
+        return -1;
+    }
 
+    public static int binarySearch(short[] array, short seek, int startpos, int endpos) {
+        if (startpos == endpos) return -1;
+        int l2 = startpos + (Math.abs(startpos - endpos) / 2);
+        if (array[l2] == seek) return l2;
+        if (seek > array[l2]) return binarySearch(array, seek, l2 + 1, endpos);
+        if (seek < array[l2]) return binarySearch(array, seek, startpos, l2);
+        return -1;
+    }
 
+    public static int binarySearch(int[] array, int seek, int startpos, int endpos) {
+        if (startpos == endpos) return -1;
+        int l2 = startpos + (Math.abs(startpos - endpos) / 2);
+        if (array[l2] == seek) return l2;
+        if (seek > array[l2]) return binarySearch(array, seek, l2 + 1, endpos);
+        if (seek < array[l2]) return binarySearch(array, seek, startpos, l2);
+        return -1;
+    }
 
+    public static int binarySearch(long[] array, long seek, int startpos, int endpos) {
+        if (startpos == endpos) return -1;
+        int l2 = startpos + (Math.abs(startpos - endpos) / 2);
+        if (array[l2] == seek) return l2;
+        if (seek > array[l2]) return binarySearch(array, seek, l2 + 1, endpos);
+        if (seek < array[l2]) return binarySearch(array, seek, startpos, l2);
+        return -1;
+    }
 
+    public static int binarySearch(float[] array, float seek, int startpos, int endpos) {
+        if (startpos == endpos) return -1;
+        int l2 = startpos + (Math.abs(startpos - endpos) / 2);
+        if (array[l2] == seek) return l2;
+        if (seek > array[l2]) return binarySearch(array, seek, l2 + 1, endpos);
+        if (seek < array[l2]) return binarySearch(array, seek, startpos, l2);
+        return -1;
+    }
 
+    public static int binarySearch(double[] array, double seek, int startpos, int endpos) {
+        if (startpos == endpos) return -1;
+        int l2 = startpos + (Math.abs(startpos - endpos) / 2);
+        if (array[l2] == seek) return l2;
+        if (seek > array[l2]) return binarySearch(array, seek, l2 + 1, endpos);
+        if (seek < array[l2]) return binarySearch(array, seek, startpos, l2);
+        return -1;
+    }
 
+    public static int binarySearch(char[] array, char seek, int startpos, int endpos) {
+        if (startpos == endpos) return -1;
+        int l2 = startpos + (Math.abs(startpos - endpos) / 2);
+        if (array[l2] == seek) return l2;
+        if (seek > array[l2]) return binarySearch(array, seek, l2 + 1, endpos);
+        if (seek < array[l2]) return binarySearch(array, seek, startpos, l2);
+        return -1;
+    }
 
+    public static <Type> int binarySearch(Type[] array, Type seek, int startpos, int endpos, Comparator<Type> typeComparator) {
+        if (startpos == endpos) return -1;
+        int l2 = startpos + (Math.abs(startpos - endpos) / 2);
+        if (typeComparator.compare(seek, array[l2]) == 0) return l2;
+        if (typeComparator.compare(seek, array[l2]) < 0)
+            return binarySearch(array, seek, l2 + 1, endpos, typeComparator);
+        if (typeComparator.compare(seek, array[l2]) > 0) return binarySearch(array, seek, startpos, l2, typeComparator);
+        return -1;
+    }
 
 
 }
+
