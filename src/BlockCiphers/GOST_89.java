bfa0df819969288886a4daef8bd6fb9b549140f7
Jewpigeon <43291850+Jewpigeon@users.noreply.github.com>
2019-05-03 15:07:33 +0300

    GOST_89 with ECB and CNT Modes

--- src/BlockCiphers/GOST_89.java
+++ src/BlockCiphers/GOST_89.java
@@ -4,24 +4,25 @@ import Basic.Cipher;
 import Basic.algorithmUtil;
 import Basic.bitUtil;
 
+import java.util.Arrays;
+
 
 public class GOST_89 implements Cipher {
     public final static byte ECB = 0;
-    public final static byte Gamming = 1;
+    public final static byte CNT = 1;
     public final static byte CFB = 2;
-    public final static byte MAC = 3;
     private static GOST_algorithm GOST;
 
     private byte MODE_SELECTED = 0;
 
     @Override
     public void setKey(byte[] key) {
-        if (key.length != 32) throw new GOST_exception("Key length must be 256 bits (32 bytes)!");
+        if (key.length != 32) throw new GOST_exception(GOST_exception.KEY_LEN);
         GOST = new GOST_algorithm(key);
     }
 
-    public void setIV(byte[] IV){
-        if(IV.length!=8) throw new GOST_exception("IV length must be 64 bits (8 bytes)!");
+    public void setIV(byte[] IV) {
+        if (IV.length != 8) throw new GOST_exception(GOST_exception.IV_LEN);
         GOST.IV = IV;
     }
 
@@ -32,8 +33,6 @@ public class GOST_89 implements Cipher {
     @Override
     public byte[] encrypt(byte[] plain) {
         switch (this.MODE_SELECTED) {
-            case 3:
-                return GOST.doMac(plain);
             case 2:
                 return GOST.encryptInCFB(plain);
             case 1:
@@ -47,8 +46,6 @@ public class GOST_89 implements Cipher {
     @Override
     public byte[] decrypt(byte[] ciph) {
         switch (this.MODE_SELECTED) {
-            case 3:
-                throw new GOST_exception("MAC can't be decrypted!");
             case 2:
                 return GOST.decryptInCFB(ciph);
             case 1:
@@ -87,149 +84,146 @@ public class GOST_89 implements Cipher {
         GOST_algorithm(byte[] K) {
             byte[][] splitted = bitUtil.splitTo4bytes(K);
             for (int i = 0; i < 8; i++) {
-                System.arraycopy(splitted[i], 0, splittedKey[i], 0,4);
-                System.arraycopy(splitted[i], 0, splittedKey[i+4], 0,4);
-                System.arraycopy(splitted[i], 0, splittedKey[i+8], 0,4);
-                System.arraycopy(splitted[i], 0, splittedKey[i+12], 0,4);
-                System.arraycopy(splitted[i], 0, splittedKey[i+16], 0,4);
-                System.arraycopy(splitted[i], 0, splittedKey[32-i-1], 0,4);
+                System.arraycopy(splitted[i], 0, splittedKey[i], 0, 4);
+                System.arraycopy(splitted[i], 0, splittedKey[i + 4], 0, 4);
+                System.arraycopy(splitted[i], 0, splittedKey[i + 8], 0, 4);
+                System.arraycopy(splitted[i], 0, splittedKey[i + 12], 0, 4);
+                System.arraycopy(splitted[i], 0, splittedKey[i + 16], 0, 4);
+                System.arraycopy(splitted[i], 0, splittedKey[32 - i - 1], 0, 4);
             }
 
         }
 
-        GOST_algorithm() { }
-
-
-
-         byte[] encryptInECB(byte[] input) {
-            if(input.length % 8 !=0) throw new GOST_exception("In ECB mode input length must be multiple of 8 (64 bits)");
-             byte[] encrypted = new byte[input.length];
-             for (int i = 0; i < input.length; i+=8) {
-                 byte[] chunck = new byte[8];
-                 System.arraycopy(input,i,chunck,0,8);
-                 byte[] A = new byte[4];
-                 byte[] B = new byte[4];
-                 System.arraycopy(chunck,0,B,0,4);
-                 System.arraycopy(chunck,4, A,0,4);
-                 for (int j = 0; j < 32; j++) {
-                     byte[] temp = A;
-                     A = bitUtil.xor(B,f(A,splittedKey[j]));
-                     B = temp;
-                 }
-                 byte[] e_chuck = new byte[8];
-                 System.arraycopy(A,0,e_chuck,0,4);
-                 System.arraycopy(B,0,e_chuck,4,4);
-                 System.arraycopy(e_chuck,0,encrypted,i,8);
-             }
-             return encrypted;
+        GOST_algorithm() {
         }
-         byte[] decryptInECB(byte[] input) {
-            if(input.length % 8 !=0) throw new GOST_exception("In ECB mode input length must be multiple of 8 (64 bits)");
+
+
+        byte[] encryptInECB(byte[] input) {
+            if (input.length % 8 != 0) throw new GOST_exception(GOST_exception.DATA_LEN);
+            byte[] encrypted = new byte[input.length];
+            for (int i = 0; i < input.length; i += 8) {
+                byte[] chunck = new byte[8];
+                System.arraycopy(input, i, chunck, 0, 8);
+                byte[] A = new byte[4];
+                byte[] B = new byte[4];
+                System.arraycopy(chunck, 0, B, 0, 4);
+                System.arraycopy(chunck, 4, A, 0, 4);
+                for (int j = 0; j < 32; j++) {
+                    byte[] temp = A;
+                    A = bitUtil.xor(B, f(A, splittedKey[j]));
+                    B = temp;
+                }
+                byte[] e_chuck = new byte[8];
+                System.arraycopy(A, 0, e_chuck, 0, 4);
+                System.arraycopy(B, 0, e_chuck, 4, 4);
+                System.arraycopy(e_chuck, 0, encrypted, i, 8);
+            }
+            return encrypted;
+        }
+        byte[] decryptInECB(byte[] input) {
+            if (input.length % 8 != 0) throw new GOST_exception(GOST_exception.DATA_LEN);
             byte[] decrypted = new byte[input.length];
             byte[][] splittedKey_reversed = splittedKey.clone();
             algorithmUtil.reverseMatrix(splittedKey_reversed);
-            for (int i = 0; i < input.length; i+=8) {
+            for (int i = 0; i < input.length; i += 8) {
                 byte[] chunck = new byte[8];
-                System.arraycopy(input,i,chunck,0,8);
+                System.arraycopy(input, i, chunck, 0, 8);
                 byte[] A = new byte[4];
                 byte[] B = new byte[4];
-                System.arraycopy(chunck,0,B,0,4);
-                System.arraycopy(chunck,4, A,0,4);
+                System.arraycopy(chunck, 0, B, 0, 4);
+                System.arraycopy(chunck, 4, A, 0, 4);
                 for (int j = 0; j < 32; j++) {
                     byte[] temp = A;
-                    A = bitUtil.xor(B,f(A,splittedKey_reversed[j]));
+                    A = bitUtil.xor(B, f(A, splittedKey_reversed[j]));
                     B = temp;
                 }
                 byte[] d_chuck = new byte[8];
-                System.arraycopy(A,0,d_chuck,0,4);
-                System.arraycopy(B,0,d_chuck,4,4);
-                System.arraycopy(d_chuck,0,decrypted,i,8);
+                System.arraycopy(A, 0, d_chuck, 0, 4);
+                System.arraycopy(B, 0, d_chuck, 4, 4);
+                System.arraycopy(d_chuck, 0, decrypted, i, 8);
             }
             return decrypted;
         }
 
 
         byte[] encryptInGamming(byte[] input) {
-            if(this.IV == null) throw new GOST_exception("Initialization Vector is not set! Set it with GOST_89.setIV(byte[] IV)");
-            byte[] gamma = generateGamma(this.IV,input.length);
-            return bitUtil.xor(input,gamma);
-        }
-
-        private byte[] generateGamma(byte[] iv,int len) {
-            byte[] iv_clone = encryptInECB(iv);
-            byte[] gamma = new byte[len];
-            while (len%8 !=0) len++;
-            byte[] gamma_extended = new byte[len];
-            byte[] N1 = new byte[4];
-            byte[] N2 = new byte[4];
-            System.arraycopy(iv_clone,0,N1,0,4);
-            System.arraycopy(iv_clone,4,N2,0,4);
-            N1 = bitUtil.intToByteArray(bitUtil.byteArrayToInt(N1) + C2);
-            N2 = bitUtil.intToByteArray(bitUtil.byteArrayToInt(N2) + C1);
-            byte[] gamma_1 = new byte[8];
-            System.arraycopy(N1,0,gamma_1,0,4);
-            System.arraycopy(N2,0,gamma_1,4,4);
-            gamma_1 = encryptInECB(gamma_1);
-            System.arraycopy(gamma_1,0,gamma_extended,0,8);
-
-            for (int i = 0; i < len-8; i+=8) {
-             N1 = new byte[4];
-             N2 = new byte[4];
-             System.arraycopy(gamma_extended,i,N1,0,4);
-             System.arraycopy(gamma_extended,i+4,N2,0,4);
-             N1 = bitUtil.intToByteArray(bitUtil.byteArrayToInt(N1) + C2);
-             N2 = bitUtil.intToByteArray(bitUtil.byteArrayToInt(N2) + C1);
-             gamma_1 = new byte[8];
-             System.arraycopy(N1,0,gamma_1,0,4);
-             System.arraycopy(N2,0,gamma_1,4,4);
-             gamma_1 = encryptInECB(gamma_1);
-             System.arraycopy(gamma_1,0,gamma_extended,i+8,8);
-            }
-            System.arraycopy(gamma_extended,0,gamma,0,gamma.length);
-            return gamma;
+            if (this.IV == null) throw new GOST_exception(GOST_exception.IV_NULL);
+            byte[] gamma = generateGamma(this.IV, input.length);
+            return bitUtil.xor(input, gamma);
         }
 
         byte[] decryptInGamming(byte[] input) {
-            if(this.IV == null) throw new GOST_exception("Initialization Vector is not set! Set it with GOST_89.setIV(byte[] IV)");
-            byte[] gamma = generateGamma(this.IV,input.length);
-            return bitUtil.xor(input,gamma);
+            if (this.IV == null) throw new GOST_exception(GOST_exception.IV_NULL);
+            byte[] gamma = generateGamma(this.IV, input.length);
+            return bitUtil.xor(input, gamma);
         }
 
 
-
-
-         byte[] encryptInCFB(byte[] input) {
-            if(this.IV == null) throw new GOST_exception("Initialization Vector is not set! Set it with GOST_89.setIV(byte[] IV)");
+        byte[] encryptInCFB(byte[] input) {
+            if (this.IV == null) throw new GOST_exception(GOST_exception.IV_NULL);
             return null;
         }
 
-
-
-         byte[] doMac(byte[] input) {
+        byte[] decryptInCFB(byte[] input) {
+            if (this.IV == null) throw new GOST_exception(GOST_exception.IV_NULL);
             return null;
         }
 
-
-
-         byte[] decryptInCFB(byte[] input) {
+        byte[] doMac(byte[] input) {
             return null;
         }
 
-
-
-         private byte[] f(byte[] A, byte[] Ki){
+        private byte[] f(byte[] A, byte[] Ki) {
             byte[] A_Ki = bitUtil.intToByteArray((bitUtil.byteArrayToInt(A) + bitUtil.byteArrayToInt(Ki)));
             A_Ki = bitUtil.splitBy4bits(A_Ki);
             for (int i = 0; i < A_Ki.length; i++) {
-                A_Ki[i] = S[i][algorithmUtil.indexOfElement(S[0], (A_Ki[i]))];
+                A_Ki[i] = S[i][algorithmUtil.binarySearch(S[0], (A_Ki[i]), 0, 16)];
             }
             A_Ki = bitUtil.concatBy4bit(A_Ki);
-            A_Ki = bitUtil.intToByteArray(bitUtil.rotateL(bitUtil.byteArrayToInt(A_Ki),11));
+            A_Ki = bitUtil.intToByteArray(bitUtil.rotateL(bitUtil.byteArrayToInt(A_Ki), 11));
             return A_Ki;
+        }
+
+        private byte[] generateGamma(byte[] iv, int len) {
+            byte[] iv_clone = encryptInECB(iv);
+            byte[] gamma = new byte[len];
+            while (len % 8 != 0) len++;
+            byte[] gamma_extended = new byte[len];
+            byte[] N1 = new byte[4];
+            byte[] N2 = new byte[4];
+            System.arraycopy(iv_clone, 0, N1, 0, 4);
+            System.arraycopy(iv_clone, 4, N2, 0, 4);
+            N1 = bitUtil.intToByteArray(bitUtil.byteArrayToInt(N1) + C2);
+            N2 = bitUtil.intToByteArray(bitUtil.byteArrayToInt(N2) + C1);
+            byte[] gamma_1 = new byte[8];
+            System.arraycopy(N1, 0, gamma_1, 0, 4);
+            System.arraycopy(N2, 0, gamma_1, 4, 4);
+            gamma_1 = encryptInECB(gamma_1);
+            System.arraycopy(gamma_1, 0, gamma_extended, 0, 8);
+
+            for (int i = 0; i < len - 8; i += 8) {
+                System.arraycopy(gamma_extended, i, N1, 0, 4);
+                System.arraycopy(gamma_extended, i + 4, N2, 0, 4);
+                N1 = bitUtil.intToByteArray(bitUtil.byteArrayToInt(N1) + C2);
+                N2 = bitUtil.intToByteArray(bitUtil.byteArrayToInt(N2) + C1);
+                System.arraycopy(N1, 0, gamma_1, 0, 4);
+                System.arraycopy(N2, 0, gamma_1, 4, 4);
+                gamma_1 = encryptInECB(gamma_1);
+                System.arraycopy(gamma_1, 0, gamma_extended, i + 8, 8);
+            }
+            System.arraycopy(gamma_extended, 0, gamma, 0, gamma.length);
+            return gamma;
         }
     }
 
     private class GOST_exception extends RuntimeException {
+        final static String IV_NULL = "Initialization Vector is not set! Set it with GOST_89.setIV(byte[] IV)";
+        final static String IV_LEN = "IV length must be 64 bits (8 bytes)!";
+        final static String DATA_LEN = "Input length must be multiple of 8 (64 bits)";
+        final static String KEY_LEN = "Key length must be 256 bits (32 bytes)!";
+        final static String MAC_DEC = "MAC can't be decrypted!";
+
+
         GOST_exception() {
             super();
         }
@@ -244,4 +238,6 @@ public class GOST_89 implements Cipher {
 
 
 }
+
+
 
