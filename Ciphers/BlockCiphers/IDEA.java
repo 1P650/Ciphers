19d4293ad58527b7a3754afb79e8db95d4e132e7
Jewpigeon <43291850+Jewpigeon@users.noreply.github.com>
2019-06-16 00:07:43 +0300

    Update IDEA.java

--- Ciphers/BlockCiphers/IDEA.java
+++ Ciphers/BlockCiphers/IDEA.java
@@ -2,9 +2,8 @@ package Ciphers.BlockCiphers;
 
 import Ciphers.Utils.BitUtil;
 
-public class IDEA extends BlockCipher {
+class IDEA extends BlockCipher {
     private IDEA_algorithm IDEA_A;
-
 
     IDEA() {
         IDEA_A = new IDEA_algorithm();
@@ -25,8 +24,6 @@ public class IDEA extends BlockCipher {
 
 
     private class IDEA_algorithm extends BlockCipherAlgorithm {
-        static final int Add = 65536;
-        static final int Mul = 65537;
         private int[][] KEY_TABLE = new int[8][6];
         private int[] KEY_TABLE_LAST = new int[4];
         private int[][] DKEY_TABLE = new int[8][6];
@@ -41,24 +38,115 @@ public class IDEA extends BlockCipher {
             super();
             generateEncryptionKeys(key);
             generateDecryptionKeys();
-
         }
 
-        //TODO Implement ECB encryption
+
         @Override
         byte[] encryptInECB(byte[] input) {
-            if (input.length % 8 != 0) throw new BlockCipherException(BlockCipherException.DATA_LEN, 8);
-            return null;
+            if (input.length % blocksize != 0) throw new BlockCipherException(BlockCipherException.DATA_LEN, blocksize);
+            short[] input_16 = BitUtil.ByteArrays.byteArrayToShortArray(input);
+            int[] D_16 = new int[input_16.length];
+            int copy_counter;
+            for (copy_counter = 0; copy_counter < input_16.length; copy_counter++) {
+                D_16[copy_counter] = input_16[copy_counter];
+            }
+            for (int i = 0; i < 8; i++) {
+                ENC_round(D_16, i);
+            }
+            int D_16_1 = D_16[1];
+            D_16[0] = BitUtil.Operation.Multiply_16(D_16[0], KEY_TABLE_LAST[0]);
+            D_16[1] = BitUtil.Operation.Add_16(D_16[2], KEY_TABLE_LAST[1]);
+            D_16[2] = BitUtil.Operation.Add_16(D_16_1, KEY_TABLE_LAST[2]);
+            D_16[3] = BitUtil.Operation.Multiply_16(D_16[3], KEY_TABLE_LAST[3]);
+
+
+            byte[] encrypted_full = BitUtil.ByteArrays.intArrayToByteArray(D_16);
+            byte[] encrypted = new byte[encrypted_full.length / 2];
+            for (int i = 2, e = 0; i < encrypted_full.length; i += 2) {
+                encrypted[e] = encrypted_full[i];
+                encrypted[e + 1] = encrypted_full[i + 1];
+                e += 2;
+                i += 2;
+
+
+            }
+
+            return encrypted;
 
         }
 
 
-        //TODO Implement ECB decryption
+
         @Override
         byte[] decryptInECB(byte[] input) {
-            if (input.length % 8 != 0) throw new BlockCipherException(BlockCipherException.DATA_LEN, 8);
-            return null;
+            if (input.length % blocksize != 0) throw new BlockCipherException(BlockCipherException.DATA_LEN, blocksize);
+            short[] input_16 = BitUtil.ByteArrays.byteArrayToShortArray(input);
+            int[] D_16 = new int[input_16.length];
+            int copy_counter;
+            for (copy_counter = 0; copy_counter < input_16.length; copy_counter++) {
+                D_16[copy_counter] = input_16[copy_counter];
+            }
+            for (int i = 0; i < 8; i++) {
+                DEC_round(D_16, i);
+            }
+            int D_16_1 = D_16[1];
+            D_16[0] = BitUtil.Operation.Multiply_16(D_16[0], DKEY_TABLE_LAST[0]);
+            D_16[1] = BitUtil.Operation.Add_16(D_16[2], DKEY_TABLE_LAST[1]);
+            D_16[2] = BitUtil.Operation.Add_16(D_16_1, DKEY_TABLE_LAST[2]);
+            D_16[3] = BitUtil.Operation.Multiply_16(D_16[3], DKEY_TABLE_LAST[3]);
 
+
+            byte[] decrypted_full = BitUtil.ByteArrays.intArrayToByteArray(D_16);
+            byte[] decrypted = new byte[decrypted_full.length / 2];
+            for (int i = 2, e = 0; i < decrypted_full.length; i += 2) {
+                decrypted[e] = decrypted_full[i];
+                decrypted[e + 1] = decrypted_full[i + 1];
+                e += 2;
+                i += 2;
+
+
+            }
+
+
+            return decrypted;
+
+
+        }
+
+        int[] ENC_round(int[] Di, int round) {
+            int A, B, C, D, E, F;
+            A = BitUtil.Operation.Multiply_16(Di[0], KEY_TABLE[round][0]);
+            B = BitUtil.Operation.Add_16(Di[1], KEY_TABLE[round][1]);
+            C = BitUtil.Operation.Add_16(Di[2], KEY_TABLE[round][2]);
+            D = BitUtil.Operation.Multiply_16(Di[3], KEY_TABLE[round][3]);
+            E = (A ^ C) & 0xFFFF;
+            F = (B ^ D) & 0xFFFF;
+
+            int F1 = BitUtil.Operation.Multiply_16(KEY_TABLE[round][5], BitUtil.Operation.Add_16(F, BitUtil.Operation.Multiply_16(E, KEY_TABLE[round][4])));
+            int F2 = BitUtil.Operation.Add_16(BitUtil.Operation.Multiply_16(E, KEY_TABLE[round][4]), F1);
+            Di[0] = (A ^ F1) & 0xFFFF;
+            Di[1] = (C ^ F1) & 0xFFFF;
+            Di[2] = (B ^ F2) & 0xFFFF;
+            Di[3] = (D ^ F2) & 0xFFFF;
+            return Di;
+        }
+
+        int[] DEC_round(int[] Di, int round) {
+            int A, B, C, D, E, F;
+            A = BitUtil.Operation.Multiply_16(Di[0], DKEY_TABLE[round][0]);
+            B = BitUtil.Operation.Add_16(Di[1], DKEY_TABLE[round][1]);
+            C = BitUtil.Operation.Add_16(Di[2], DKEY_TABLE[round][2]);
+            D = BitUtil.Operation.Multiply_16(Di[3], DKEY_TABLE[round][3]);
+            E = (A ^ C) & 0xFFFF;
+            F = (B ^ D) & 0xFFFF;
+
+            int F1 = BitUtil.Operation.Multiply_16(DKEY_TABLE[round][5], BitUtil.Operation.Add_16(F, BitUtil.Operation.Multiply_16(E, DKEY_TABLE[round][4])));
+            int F2 = BitUtil.Operation.Add_16(BitUtil.Operation.Multiply_16(E, DKEY_TABLE[round][4]), F1);
+            Di[0] = (A ^ F1) & 0xFFFF;
+            Di[1] = (C ^ F1) & 0xFFFF;
+            Di[2] = (B ^ F2) & 0xFFFF;
+            Di[3] = (D ^ F2) & 0xFFFF;
+            return Di;
         }
 
 
@@ -77,13 +165,13 @@ public class IDEA extends BlockCipher {
             int h = 1;
             int k = 2;
 
-            for (int i = 0; i < 52; i += 8, j += 8) {
-                bytes = BitUtil.Rotation.rotate128L(bytes, 25);
+            for (int i = 0; i < 7; i++, j += 8) {
+                bytes = BitUtil.Rotation.rotate128L(bytes.clone(), 25);
                 short[] tmp = BitUtil.ByteArrays.byteArrayToShortArray(bytes);
                 System.arraycopy(tmp, 0, subkeys, j, 8);
                 System.arraycopy(subkeys, j - k, subkeys6[h++], 0, 6);
                 k += 2;
-                if (i == 48) {
+                if (i == 6) {
                     tmp = BitUtil.ByteArrays.byteArrayToShortArray(BitUtil.Rotation.rotate128R(bytes, 25));
                     System.arraycopy(tmp, 0, last, 0, 4);
                 }
@@ -92,48 +180,43 @@ public class IDEA extends BlockCipher {
 
             for (int i = 0; i < 8; i++) {
                 for (int l = 0; l < 6; l++) {
-                    KEY_TABLE[i][l] = subkeys6[i][l];
-                    KEY_TABLE[i][l] &= 0xffff;
+                    KEY_TABLE[i][l] = ((int) subkeys6[i][l]) & 0xffff;
                 }
             }
 
             for (int i = 0; i < 4; i++) {
-                KEY_TABLE_LAST[i] = last[i];
-                KEY_TABLE_LAST[i] &= 0xffff;
+                KEY_TABLE_LAST[i] = ((int) last[i]) & 0xffff;
             }
+
 
         }
 
         private void generateDecryptionKeys() {
             int j = 7;
-            int[][] DEC_subkeys6 = new int[8][6];
-            int[] DEC_last = new int[4];
-            DEC_subkeys6[0][0] = BitUtil.Operation.mInver(KEY_TABLE_LAST[0], 65537);
-            DEC_subkeys6[0][1] = BitUtil.Operation.addInver(KEY_TABLE_LAST[1], 65536);
-            DEC_subkeys6[0][2] = BitUtil.Operation.addInver(KEY_TABLE_LAST[2], 65536);
-            DEC_subkeys6[0][3] = BitUtil.Operation.mInver(KEY_TABLE_LAST[3], 65537);
-            DEC_subkeys6[0][4] = KEY_TABLE[7][4];
-            DEC_subkeys6[0][5] = KEY_TABLE[7][5];
+            DKEY_TABLE[0][0] = BitUtil.Operation.MultiplicativeInverse(KEY_TABLE_LAST[0], 65537);
+            DKEY_TABLE[0][1] = BitUtil.Operation.AdditiveInverse(KEY_TABLE_LAST[1], 65536);
+            DKEY_TABLE[0][2] = BitUtil.Operation.AdditiveInverse(KEY_TABLE_LAST[2], 65536);
+            DKEY_TABLE[0][3] = BitUtil.Operation.MultiplicativeInverse(KEY_TABLE_LAST[3], 65537);
+            DKEY_TABLE[0][4] = KEY_TABLE[7][4];
+            DKEY_TABLE[0][5] = KEY_TABLE[7][5];
 
             for (int i = 1; i < 8; i++) {
-                DEC_subkeys6[i][0] = BitUtil.Operation.mInver(KEY_TABLE[j][0], 65537);
-                DEC_subkeys6[i][1] = BitUtil.Operation.addInver(KEY_TABLE[j][2], 65536);
-                DEC_subkeys6[i][2] = BitUtil.Operation.addInver(KEY_TABLE[j][1], 65536);
-                DEC_subkeys6[i][3] = BitUtil.Operation.mInver(KEY_TABLE[j][3], 65537);
+                DKEY_TABLE[i][0] = BitUtil.Operation.MultiplicativeInverse(KEY_TABLE[j][0], 65537);
+                DKEY_TABLE[i][1] = BitUtil.Operation.AdditiveInverse(KEY_TABLE[j][2], 65536);
+                DKEY_TABLE[i][2] = BitUtil.Operation.AdditiveInverse(KEY_TABLE[j][1], 65536);
+                DKEY_TABLE[i][3] = BitUtil.Operation.MultiplicativeInverse(KEY_TABLE[j][3], 65537);
                 j--;
-                DEC_subkeys6[i][4] = KEY_TABLE[j][4];
-                DEC_subkeys6[i][5] = KEY_TABLE[j][5];
+                DKEY_TABLE[i][4] = KEY_TABLE[j][4];
+                DKEY_TABLE[i][5] = KEY_TABLE[j][5];
+                if (j == 0) {
+                    DKEY_TABLE_LAST[0] = BitUtil.Operation.MultiplicativeInverse(KEY_TABLE[0][0], 65537);
+                    DKEY_TABLE_LAST[1] = BitUtil.Operation.AdditiveInverse(KEY_TABLE[0][1], 65536);
+                    DKEY_TABLE_LAST[2] = BitUtil.Operation.AdditiveInverse(KEY_TABLE[0][2], 65536);
+                    DKEY_TABLE_LAST[3] = BitUtil.Operation.MultiplicativeInverse(KEY_TABLE[0][3], 65537);
 
+                }
             }
 
-            DEC_last[0] = BitUtil.Operation.mInver(KEY_TABLE[0][0], 65537);
-            DEC_last[1] = BitUtil.Operation.addInver(KEY_TABLE[0][1], 65536);
-            DEC_last[2] = BitUtil.Operation.addInver(KEY_TABLE[0][2], 65536);
-            DEC_last[3] = BitUtil.Operation.mInver(KEY_TABLE[0][3], 65537);
-
-
-            DKEY_TABLE = DEC_subkeys6;
-            DKEY_TABLE_LAST = DEC_last;
         }
 
 
