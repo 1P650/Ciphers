bcc652c13707842ad739cb6bf391a82ab664dfdd
Jewpigeon <43291850+Jewpigeon@users.noreply.github.com>
2019-06-21 02:41:35 +0300

    Update BlockCiphersList.java

--- Ciphers/BlockCiphers/BlockCiphersList.java
+++ Ciphers/BlockCiphers/BlockCiphersList.java
@@ -7,9 +7,8 @@ public interface BlockCiphersList {
     static BlockCipher IDEA(){ return new IDEA();}
     static BlockCipher GOST(){ return new GOST89();}
     static BlockCipher TEA(){ return new TEA();}
-    static BlockCipher Twofish() {return new Twofish();}
     static BlockCipher CAST5(){return new CAST5();}
-    static BlockCipher CAST6(){return new CAST6();}
+    
 
 
 
