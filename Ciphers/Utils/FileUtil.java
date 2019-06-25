de6e28adef70c78317f7573417d7bd4d35f98e62
Jewpigeon <43291850+Jewpigeon@users.noreply.github.com>
2019-06-26 00:54:18 +0300

    Update FileUtil.java

--- Ciphers/Utils/FileUtil.java
+++ Ciphers/Utils/FileUtil.java
@@ -1,4 +1,22 @@
 package Ciphers.Utils;
 
 public class FileUtil {
+  
+    public static final class Read{
+
+    }
+
+    public static final class Write{
+
+    }
+
+    public static final class Metadata{
+
+
+    }
+
+    public static final class Operations{
+
+
+    }
 }
