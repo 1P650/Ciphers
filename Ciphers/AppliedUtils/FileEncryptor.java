ffe54fe6297ac8ca83716230a791d920e8b9c54f
Jewpigeon <43291850+Jewpigeon@users.noreply.github.com>
2019-06-26 00:53:55 +0300

    Update FileEncryptor.java

--- Ciphers/AppliedUtils/FileEncryptor.java
+++ Ciphers/AppliedUtils/FileEncryptor.java
@@ -1,4 +1,28 @@
 package Ciphers.AppliedUtils;
 
 public class FileEncryptor {
+    public byte[] encrypt(java.io.File file, Cipher cipher){return null;}
+    public byte[] decrypt(java.io.File file, Cipher cipher){return null;}
+    public byte[] encrypt(java.io.File[] file, Cipher cipher){return null;}
+    public byte[] decrypt(java.io.File[] file, Cipher cipher){return null;}
+    public byte[] encrypt(java.io.File file, CipherCascade cipherCascade){return null;}
+    public byte[] decrypt(java.io.File file, CipherCascade cipherCascade){return null;}
+
+
+    public java.io.File encryptInFile(java.io.File file,Cipher cipher){return null;}
+    public java.io.File decryptInFile(java.io.File file, Cipher cipher){return null;}
+    public java.io.File encryptInFile(java.io.File[] file, Cipher cipher){return null;}
+    public java.io.File decryptInFile(java.io.File[] file, Cipher cipher){return null;}
+    public java.io.File encryptInFile(java.io.File file, CipherCascade cipherCascade){return null;}
+    public java.io.File decryptInFile(java.io.File file, CipherCascade cipherCascade){return null;}
+
+    public java.io.File encryptInFile(java.io.FileInputStream fis,Cipher cipher){return null;}
+    public java.io.File decryptInFile(java.io.FileInputStream fis, Cipher cipher){return null;}
+    public java.io.File encryptInFile(java.io.FileInputStream[] fis, Cipher cipher){return null;}
+    public java.io.File decryptInFile(java.io.FileInputStream[] fis, Cipher cipher){return null;}
+    public java.io.File encryptInFile(java.io.FileInputStream fis, CipherCascade cipherCascade){return null;}
+    public java.io.File decryptInFile(java.io.FileInputStream fis, CipherCascade cipherCascade){return null;}
+
+    public byte[] Hash(java.io.File file, HashFunction hashFunction){return null;}
+    public byte[] Hash(java.io.FileInputStream fis, HashFunction hashFunction){return null;}
 }
